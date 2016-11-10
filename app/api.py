e147f8484826fefa4e00bc6297ab6d73aa2f5d7b
Nikhil Wahi <nikhilwahi1995@gmail.com>
2016-11-10 12:06:41 -0600

    add two image support

--- app/api.py
+++ app/api.py
@@ -1,13 +1,26 @@
 #!flask/bin/python
 from flask import jsonify
+import requests
+from bs4 import BeautifulSoup
 import models
 from models import user_model, movie_model, actor_model
 import hashlib
 import urllib2
 import json
+import httplib
+import urllib
+import base64
 import sys
 reload(sys)
+import re
+
 sys.setdefaultencoding('utf-8')
+
+HOST = "127.0.0.1"
+USER = "root"
+PASSWORD = "cs411fa2016"
+DB = "imdb"
+
 
 def add_user(json_body):
     existing, message = user_model.check_existing_user(json_body['EMAIL'])
@@ -48,15 +61,43 @@ def get_movie_by_id(movie_id):
     success, movie, message = movie_model.get_movie_by_id(movie_id)
     actors = None
     if success:
-        movie['Poster'] = get_movie_image_url(movie)
+        cover_url, thumb_url = get_movie_image_url(movie)
+        movie['Poster'] = cover_url
+        movie['Thumbnail'] = thumb_url
         actors = actor_model.get_actors_by_movie_id(movie_id)
     return jsonify({'success': success, 'movie': movie, 'actors': actors, 'message': message})
 
+
 def get_movie_image_url(movie):
-    base = "http://www.omdbapi.com/?t="
-    movie_info = urllib2.urlopen(base+movie['Name']).read()
-    movie_info = json.loads(movie_info)
-    return movie_info["Poster"].encode('ISO-8859-1')
+    headers = {
+        # Request headers
+        'Ocp-Apim-Subscription-Key': '09a829ae3d0d4e07b0cf605391d2a469',
+    }
+    params = urllib.urlencode({
+        # Request parameters
+        'q': movie['Name'] + " movie poster",
+        'count': '1',
+        'offset': '0',
+        'mkt': 'en-us',
+        'safeSearch': 'Moderate',
+        'size': 'Large'
+    })
+    cover_url = ""
+
+    try:
+        conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+        conn.request("GET", "/bing/v5.0/images/search?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        cover_url = json.loads(data)['value'][0]["contentUrl"].encode('ISO-8859-1')
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    omdb_base = "http://www.omdbapi.com/?t="
+    movie_info_omdb = urllib2.urlopen(omdb_base + movie['Name']).read()
+    movie_info_omdb = json.loads(movie_info_omdb)
+    return cover_url, movie_info_omdb["Poster"].encode('ISO-8859-1')
+
 
 def get_movies_by_name_search(search_string):
     success, movies, message = movie_model.get_movies_by_name_search(search_string)
